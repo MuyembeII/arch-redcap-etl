d6d6cd53e0a37957b28983ef2c82d53d2d3a17b2
GMuyembe Jr <muyembegift@gmail.com>
2025-02-07 20:07:38 +0200

    WRA Visit Events

--- src/Database/sql/tables/tbl_visits.sql
+++ src/Database/sql/tables/tbl_visits.sql
@@ -4,15 +4,16 @@ CREATE TABLE arch_etl_db.visit
     id           INT            NOT NULL primary key AUTO_INCREMENT,
     visit_id     INT            NOT NULL,
     visit_number DECIMAL(10, 1) NOT NULL,
-    visit_name   VARCHAR(255)   NOT NULL,
-    visit_alias  VARCHAR(255)   NOT NULL
+    visit_name   VARCHAR(64)   NOT NULL,
+    visit_alias  VARCHAR(64)   NOT NULL
 );
 CREATE INDEX visit_number_idx ON arch_etl_db.visit (visit_number);
 CREATE INDEX visit_name_idx ON arch_etl_db.visit (visit_name);
-
+-- Seed visit details
 INSERT INTO arch_etl_db.visit(visit_id, visit_number, visit_name, visit_alias)
 VALUES (98, 1.0, 'wra_baseline_arm_1', 'WRA Baseline'),
        (99, 2.0, 'wra_followup_visit_arm_1', 'WRA Follow-Up Visit'),
        (116, 3.0, 'wra_followup_visit_arm_1b', 'WRA Follow-Up Visit 2'),
        (117, 4.0, 'wra_followup_visit_arm_1c', 'WRA Follow-Up Visit 3'),
-       (117, 5.0, 'wra_followup_visit_arm_1d', 'WRA Follow-Up Visit 4');
+       (154, 5.0, 'wra_followup_visit_arm_1d', 'WRA Follow-Up Visit 4'),
+       (162, 6.0, 'wra_followup_visit_arm_1e', 'WRA Follow-Up Visit 5');
